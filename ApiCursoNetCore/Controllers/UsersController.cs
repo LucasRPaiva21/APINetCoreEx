f7b65b76c588fb6767f7f83737e068ad440482b0
Lucas Paiva <lucas.ramalhopaiva@hotmail.com>
2021-12-21 18:36:13 -0300

    69. Api.Application - Criar Método GetAll [HttpGet]

--- ApiCursoNetCore/Controllers/UsersController.cs
+++ ApiCursoNetCore/Controllers/UsersController.cs
@@ -1,16 +1,32 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using APICursoNetCore.Domain.Interfaces.Services.User;
+using Microsoft.AspNetCore.Mvc;
 using System;
-using System.Collections.Generic;
-using System.Linq;
+using System.Net;
 using System.Threading.Tasks;
 
 namespace ApiCursoNetCore.Controllers
 {
-    public class UsersController : Controller
+    [ApiController]
+    [Route ("api/[controller]")]
+    public class UsersController : ControllerBase
     {
-        public IActionResult Index()
+       [HttpGet]
+       public async Task<ActionResult> GetAll([FromServices] IUserService service)
         {
-            return View();
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            try
+            {
+                return Ok(await service.GetAll());
+            }
+            catch (ArgumentException e)
+            {
+
+                return StatusCode((int)HttpStatusCode.InternalServerError, e.Message);
+            }
         }
     }
 }
