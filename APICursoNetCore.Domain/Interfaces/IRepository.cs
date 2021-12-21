abb84f4495d1ce198be93b22ca32868a3d267328
Lucas Paiva <lucas.ramalhopaiva@hotmail.com>
2021-12-21 17:14:39 -0300

    59. Api.Domain Criar a interface de Repository(IRepository)

---
+++ APICursoNetCore.Domain/Interfaces/IRepository.cs
@@ -0,0 +1,20 @@
+﻿using APICursoNetCore.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace APICursoNetCore.Domain.Interfaces
+{
+    public interface IRepository<T> where T:BaseEntity
+    {
+        Task<T> InsertAsync(T item);
+
+        Task<T> UpdateAsync(T item);
+
+        Task<bool> DeleteAsync(Guid id);
+
+        Task<T> SelectAsync(Guid id);
+
+        Task<IEnumerable<T>> SelectAsync();
+    }
+}
