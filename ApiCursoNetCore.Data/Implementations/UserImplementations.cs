d07daf0816b02b1d6da1197a44c26035aa3040c1
Lucas Paiva <lucas.ramalhopaiva@hotmail.com>
2021-12-22 20:00:22 -0300

    79. Api.Data - UserImplementation

---
+++ ApiCursoNetCore.Data/Implementations/UserImplementations.cs
@@ -0,0 +1,24 @@
+﻿using ApiCursoNetCore.Data.Context;
+using ApiCursoNetCore.Data.Repository;
+using APICursoNetCore.Domain.Entities;
+using APICursoNetCore.Domain.Repository;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+
+namespace ApiCursoNetCore.Data.Implementations
+{
+    public class UserImplementations : BaseRepository<UserEntity>, IUserRepository
+    {
+        private DbSet<UserEntity> _dataset;
+
+        public UserImplementations(MyContext context) : base(context)
+        {
+            _dataset = context.Set<UserEntity>();
+        }
+
+        public async Task<UserEntity> FindByLogin(string email)
+        {
+            return await _dataset.FirstOrDefaultAsync(u => u.Email.Equals(email));
+        }
+    }
+}
