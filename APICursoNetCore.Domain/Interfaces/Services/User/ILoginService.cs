9c49d91a2a392c02624f9a55f6d28558ff11f9c9
Lucas Paiva <lucas.ramalhopaiva@hotmail.com>
2021-12-22 20:02:11 -0300

    80. Api.Domain - ILoginService

---
+++ APICursoNetCore.Domain/Interfaces/Services/User/ILoginService.cs
@@ -0,0 +1,10 @@
+﻿using APICursoNetCore.Domain.Entities;
+using System.Threading.Tasks;
+
+namespace APICursoNetCore.Domain.Interfaces.Services.User
+{
+    public interface ILoginService
+    {
+        Task<object> FindByLogin(UserEntity user);
+    }
+}
