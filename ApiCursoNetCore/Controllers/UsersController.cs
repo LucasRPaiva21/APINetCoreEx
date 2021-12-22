fe74e88d6678136ac1ef59a086d8b9afd81a6798
Lucas Paiva <lucas.ramalhopaiva@hotmail.com>
2021-12-22 16:58:55 -0300

    73. Api.Application - Criar Método Put [HttpPut]

--- ApiCursoNetCore/Controllers/UsersController.cs
+++ ApiCursoNetCore/Controllers/UsersController.cs
@@ -84,6 +84,33 @@ namespace ApiCursoNetCore.Controllers
             }
         }
 
+        [HttpPut]
+        public async Task<ActionResult> Put([FromBody] UserEntity user)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            try
+            {
+                var result = await _service.Put(user);
+                if (result != null)
+                {
+                    return Ok(result);
+                }
+                else
+                {
+                    return BadRequest();
+                }
+            }
+            catch (ArgumentException e)
+            {
+
+                return StatusCode((int)HttpStatusCode.InternalServerError, e.Message);
+            }
+        }
+
 
     }
 }
