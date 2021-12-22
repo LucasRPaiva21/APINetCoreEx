939616e5783e49ed3d409473c6cd1e82c363b9c0
Lucas Paiva <lucas.ramalhopaiva@hotmail.com>
2021-12-22 17:23:26 -0300

    75. Api.Application - Implementar Swagger

--- ApiCursoNetCore/Startup.cs
+++ ApiCursoNetCore/Startup.cs
@@ -6,6 +6,8 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
+using System;
 
 namespace ApiCursoNetCore
 {
@@ -25,6 +27,27 @@ namespace ApiCursoNetCore
             ConfigureRepository.ConfigureDependenciesRepository(services);
 
             services.AddControllers();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Version = "v1",
+                    Title = "Curso de API com AspNetCore 3.1 - Na Prática",
+                    Description = "Arquitetura DDD",
+                    TermsOfService = new Uri("http://www.mfrinfo.com.br"),
+                    Contact = new OpenApiContact
+                    {
+                        Name = "Lucas Ramalho Paiva",
+                        Email = "lucas.ramalhopaiva@hotmail.com",
+                        Url = new Uri("http://www.mrfinfo.com.br")
+                    },
+                    License = new OpenApiLicense
+                    {
+                        Name = "Termo de Licença de Uso"
+                    }
+                }
+                );
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -36,6 +59,13 @@ namespace ApiCursoNetCore
             }
 
             app.UseHttpsRedirection();
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+           {
+               c.SwaggerEndpoint("/swagger/v1/swagger.json", "Curso de API com AspNetCore 3.1");
+               c.RoutePrefix = string.Empty;
+           });
 
             app.UseRouting();
 
