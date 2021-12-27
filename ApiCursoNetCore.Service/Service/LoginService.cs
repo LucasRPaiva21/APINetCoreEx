2023fb9583081ba011715a0a7df12879c1a94f1b
Lucas Paiva <lucas.ramalhopaiva@hotmail.com>
2021-12-27 18:25:40 -0300

    81. Api.Service - LoginService

---
+++ ApiCursoNetCore.Service/Service/LoginService.cs
@@ -0,0 +1,31 @@
+﻿using APICursoNetCore.Domain.Entities;
+using APICursoNetCore.Domain.Interfaces.Services.User;
+using APICursoNetCore.Domain.Repository;
+using System.Threading.Tasks;
+
+namespace ApiCursoNetCore.Service.Service
+{
+    public class LoginService : ILoginService
+    {
+        private IUserRepository _repository;
+
+        public LoginService(IUserRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<object> FindByLogin(UserEntity user)
+        {
+            var baseUser = new UserEntity();
+
+            if(user != null && !string.IsNullOrEmpty(user.Email))
+            {
+                return await _repository.FindByLogin(user.Email);
+            }
+            else
+            {
+                return null;
+            }
+        }
+    }
+}
