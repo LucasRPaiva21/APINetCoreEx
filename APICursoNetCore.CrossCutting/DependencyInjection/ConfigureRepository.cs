e5ffef97f7e13ac97bdb24381856eb53f9f22aea
Lucas Paiva <lucas.ramalhopaiva@hotmail.com>
2021-12-27 18:53:36 -0300

    82. Api.Application - LoginController

--- APICursoNetCore.CrossCutting/DependencyInjection/ConfigureRepository.cs
+++ APICursoNetCore.CrossCutting/DependencyInjection/ConfigureRepository.cs
@@ -1,6 +1,8 @@
 ﻿using ApiCursoNetCore.Data.Context;
+using ApiCursoNetCore.Data.Implementations;
 using ApiCursoNetCore.Data.Repository;
 using APICursoNetCore.Domain.Interfaces;
+using APICursoNetCore.Domain.Repository;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -11,6 +13,7 @@ namespace APICursoNetCore.CrossCutting.DependencyInjection
         public static void ConfigureDependenciesRepository(IServiceCollection serviceCollection)
         {
             serviceCollection.AddScoped(typeof(IRepository<>), typeof(BaseRepository<>));
+            serviceCollection.AddScoped<IUserRepository, UserImplementations>();
 
             serviceCollection.AddDbContext<MyContext>(
                 options => options.UseMySql("Server=localhost;Port=3306;Database=dbAPI;Uid=root;Pwd=12345678")
--- APICursoNetCore.CrossCutting/DependencyInjection/ConfigureService.cs
+++ APICursoNetCore.CrossCutting/DependencyInjection/ConfigureService.cs
@@ -9,6 +9,7 @@ namespace APICursoNetCore.CrossCutting.DependencyInjection
         public static void ConfigureDependenciesService(IServiceCollection serviceCollection)
         {
             serviceCollection.AddTransient<IUserService, UserService>();
+            serviceCollection.AddTransient<ILoginService, LoginService>();
         }
     }
 }
