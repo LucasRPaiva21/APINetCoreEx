cea3d28c07da2cff8e3b7f3227206ad5f48bcf70
Lucas Paiva <lucas.ramalhopaiva@hotmail.com>
2021-12-27 18:34:44 -0300

    83. Api.CrossCutting - Injeção de Dependências

---
+++ ApiCursoNetCore/Controllers/LoginController.cs
@@ -0,0 +1,49 @@
+﻿using APICursoNetCore.Domain.Entities;
+using APICursoNetCore.Domain.Interfaces.Services.User;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace ApiCursoNetCore.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class LoginController : ControllerBase
+    {
+       
+        [HttpPost]
+        public async Task<object> Login([FromBody] UserEntity userEntity, [FromServices] ILoginService service)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if(userEntity == null)
+            {
+                return BadRequest();
+            }
+
+            try
+            {
+                var result = await service.FindByLogin(userEntity);
+                if(result != null)
+                {
+                    return Ok(result);
+                }
+                else
+                {
+                    return NotFound();
+                }
+            }
+            catch (ArgumentException ex)
+            {
+
+                return StatusCode((int)HttpStatusCode.InternalServerError, ex.Message);
+            }
+        }       
+    }
+}
