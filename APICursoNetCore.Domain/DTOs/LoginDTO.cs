f5f9df05e19b339ab34efd503d0aaf5d5f29f1a9
Lucas Paiva <lucas.ramalhopaiva@hotmail.com>
2021-12-27 19:00:47 -0300

    84. Api.Domain - Criar Login Dto (Data Transfer Object)

---
+++ APICursoNetCore.Domain/DTOs/LoginDTO.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace APICursoNetCore.Domain.DTOs
+{
+    public class LoginDTO
+    {
+        [EmailAddress(ErrorMessage = "Email em formato inválido")]
+        [Required(ErrorMessage = "Email é um campo obrigatório para o Login")]      
+        [StringLength(100, ErrorMessage = "Email deve ter no máximo {1} carateres.")]
+        public string Email { get; set; }
+    }
+}
