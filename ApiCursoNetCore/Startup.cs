9c57574ab19d738f039a39ce2ff14834da78b5d5
Lucas Paiva <lucas.ramalhopaiva@hotmail.com>
2022-01-25 20:10:27 -0300

    91. Api.Authorization - Adicionar Botão authoriza no Swagger

--- ApiCursoNetCore/Startup.cs
+++ ApiCursoNetCore/Startup.cs
@@ -12,6 +12,7 @@ using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Options;
 using Microsoft.OpenApi.Models;
 using System;
+using System.Collections.Generic;
 
 namespace ApiCursoNetCore
 {
@@ -83,6 +84,30 @@ namespace ApiCursoNetCore
                     }
                 }
                 );
+
+                c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    Description = "Entre com o Token JWT",
+                    Name = "Authorization",
+                    In = ParameterLocation.Header,
+                    Type = SecuritySchemeType.ApiKey
+                });
+
+                c.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference
+                            {
+                                Id = "Bearer",
+                                Type = ReferenceType.SecurityScheme
+                            }
+                        },
+                        new List<string>()
+                    }
+
+                });
             });
         }
 
